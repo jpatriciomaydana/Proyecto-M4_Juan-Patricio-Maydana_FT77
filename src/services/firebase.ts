7e3bf3853efba54eb1d1406c446e18ee69c53ccf
jpatriciomaydana <maydanajp@gmail.com>
2026-09-16 17:10:54 -0300

    feat agregar firebase como dependencia, carpeta services

---
+++ src/services/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app"; //conecta con servidores firebase//
+import { getAuth } from "firebase/auth"; //manejar usuarios //
+import { getFirestore } from "firebase/firestore"; //bases de datos en tiempo real //
+
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+//inizialize app//
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);        //autorización
+export const db = getFirestore(app);       //base de datos
