f5afa6c0cbff2baf5a70411829d803fff02218f3
jpatriciomaydana <maydanajp@gmail.com>
2026-09-17 14:00:14 -0300

    feat: envolver app.tsx con authprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { AuthProvider } from "./context/authContext";
+
+
 function App() {
   return (
-    <main>
-      <h1>MateCode</h1>
-      <p>Gestión de tareas para pequeñas empresas</p>
-    </main>
+    <AuthProvider>
+      <main>
+        <h1>MateCode</h1>
+        <p>Gestión de tareas para pequeñas empresas</p>
+      </main>
+    </AuthProvider>
   )
 }
 
